41d21da2c9f5e549bae0beb64f7b1459f923e02e
kimhyeonsu <103178084+kimhyeons@users.noreply.github.com>
2026-02-04 15:14:10 +0900

    cooldown ui

---
+++ app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.pauselock"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "com.example.pauselock"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    // Room DB 라이브러리
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+}
---
+++ app/src/main/java/com/example/pauselock/PhisingEvent.java
@@ -0,0 +1,17 @@
+package com.example.pauselock;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "phishing_logs")
+public class PhisingEvent {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public long timestamp;      //발생 시간
+    public float shakeIntensity;        //손떨림 수치
+    public int backspaceCount;      //타자 오타 횟수
+    public boolean isOnCall;        //통화 중 여부
+    public String currentApp;       //현재 실행 중인 앱
+
+}
